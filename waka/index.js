b51072c903c225df6cf5ed7d7c6cc7eef34ddab6
Yamrc <yamrci@outlook.com>
2026-03-07 17:34:01 +0800

    Feat: 新增卡片

--- waka/index.js
+++ waka/index.js
@@ -1,9 +1,8 @@
-import 'dotenv/config';
 import { WakaClient } from 'wakaclient';
 import { Octokit } from '@octokit/rest';
 
-const { GIST_ID, GH_PAT, WAKATIME_KEY } = process.env;
-if (!GIST_ID || !GH_PAT || !WAKATIME_KEY)
+const { GIST_ID_1, GIST_ID_2, GH_PAT, WAKATIME_KEY } = process.env;
+if (!GIST_ID_1 || !GIST_ID_2 || !GH_PAT || !WAKATIME_KEY)
   throw new Error('Missing environment variables');
 
 const waka = new WakaClient(WAKATIME_KEY);
@@ -13,29 +12,50 @@ const octo = new Octokit({ auth: `token ${GH_PAT}` });
   try {
     const data = (await waka.getStats()).data;
 
-    const topLanguage = data.languages.filter(l => l.name !== 'Other')[0];
-    const topEditor = data.editors.filter(e => e.name !== 'Other')[0];
-    const topOS = data.operating_systems.filter(os => os.name !== 'Other')[0];
+    const total = data.human_readable_total_including_other_language;
+    const avg = data.human_readable_daily_average_including_other_language;
 
-    const lines = [
-      `Most Used Language: ${topLanguage.name} (${topLanguage.text})`,
-      `Most Used IDE: ${topEditor.name} (${topEditor.text})`,
-      `Most Used OS: ${topOS.name} (${topOS.text})`,
+    const language = excludeOther(data.languages);
+    const ide = excludeOther(data.editors);
+    const os = excludeOther(data.operating_systems);
+
+    const best_day = data.best_day;
+    const minus_holidays = data.days_minus_holidays;
+    const lines = `${data.human_additions} insertions(+) ${data.human_deletions} deletions(-)`
+
+    const profile = [
+      `Most Used language: ${language[0].name} (${language[0].text})`,
+      `Most Used IDE: ${ide[0].name} (${ide[0].text})`,
+      `Most Used OS: ${os[0].name} (${os[0].text})`,
+    ];
+    const stat = [
+      `Best day: ${best_day.text} (${best_day.date})`,
+      `Active days: ${minus_holidays}`,
+      `Lines: ${lines}`
     ];
 
-    const gist = await octo.gists.get({ gist_id: GIST_ID });
-    const filename = Object.keys(gist.data.files)[0];
-    await octo.gists.update({
-      gist_id: GIST_ID,
-      description: `Total Time Coding ${data.human_readable_total_including_other_language}`,
-      files: {
-        [filename]: {
-          filename: 'profile.md',
-          content: lines.join('\n'),
-        },
-      },
-    });
-  } catch (error) {
-    console.error('Failed to update gist:', error);
-  }
+    await Promise.all([
+      updateGist(GIST_ID_1, `Total Time Coding ${total}`, "profile.yml", profile),
+      updateGist(GIST_ID_2, `Daily Average ${avg}`, "stat.yml", stat)
+    ]);
+  } catch (error) { console.error(`Something went wrong:`, error.message); }
 })();
+
+async function updateGist(gist_id, description, file_name, file_content) {
+  try {
+    await octo.gists.update({
+      gist_id,
+      description,
+      files: {
+        [file_name]: {
+          content: file_content.join('\n'),
+        }
+      }
+    });
+    console.log(`Updated ${file_name}(${gist_id}).`)
+  } catch (error) { console.error(`Update failed ${file_name}(${gist_id}):`, error.message); }
+}
+
+function excludeOther (cont) {
+  return cont.filter(s => s.name !== "Other");
+}
